003b3744ce022521e30bb058c8fc14c146a33d98
monique.dingding <monique.dingding@lamudi.com>
2021-12-17 03:43:19 +0800

    feat: add inline button to request contact

--- index.js
+++ index.js
@@ -16,26 +16,50 @@ const init = async () => {
   console.log(res.data)
 }
 
-app.post(URI, async (req, res) => {
-  console.log(req.body)
+let step = 0
 
-  const payload = {
-    chat_id: req.body.message.chat.id,
-    text: req.body.message.text
-  }
+// TODO: move to other file
+const processBot = ({ message }) => {
+  let payload = {};
 
-  const msgPayload = {
-    chat_id: req.body.message.chat.id,
-    message_id: req.body.message.message_id,
-    text: "Welcome to Group 5 Chatbot JS! Are you ready to start?",
-    reply_markup: {
-      keyboard: [
-        [{ text: "Yes, let\'s do it!" }]
-      ]
+  if (step === 0 || message === "/logout") {
+    step = 0
+
+    payload = {
+      chat_id: message.chat.id,
+      message_id: message.message_id,
+      text: "To start, please type /start."
     }
   }
 
-  await axios.post(`${TELEGRAM_API}/sendMessage`, msgPayload)
+  if (message.text === "/start") {
+    step++
+    payload = {
+      chat_id: message.chat.id,
+      message_id: message.message_id,
+      text: "Welcome to Group 5 Chatbot JS! Are you ready to start?",
+      reply_markup: {
+        keyboard: [
+          [{ 
+            text: "Yes, let\'s do it!",
+            request_contact: true
+          }]
+        ],
+        one_time_keyboard: true
+      }
+    }
+  }
+
+  return payload
+} 
+
+
+app.post(URI, async (req, res) => {
+  console.log(req.body)
+
+ const payload = processBot(req.body)
+
+  await axios.post(`${TELEGRAM_API}/sendMessage`, payload)
 
   return res.send()
 })
